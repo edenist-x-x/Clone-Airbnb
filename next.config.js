40b27cca0b7c9e7eec281c151f29ae3c891c92f9
edenist-x-x <yaron_e5@naver.com>
2021-09-29 12:34:48 +0900

    .

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// next configure file
+
+module.exports = {
+  images: {
+    domains: ["links.papareact.com"],
+  },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoiamFlZGVuaXN0IiwiYSI6ImNrdGhldjUyZjAxcngydW1zejU2bWs1dnMifQ.K3Ijqq6Vi7UKKrZSwWT-uw",
+  },
+};
